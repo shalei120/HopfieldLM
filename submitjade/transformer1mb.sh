c38e16193de4aa050ace13572ff01eb1e517ded9
shalei120 <shalei120@sina.com>
2021-04-07 22:24:17 +0000

    dgx change

--- submitjade/transformer1mb.sh
+++ submitjade/transformer1mb.sh
@@ -10,4 +10,4 @@ module load cuda/9.2
 #nvidia-smi
 echo $PWD
 # run the application
-python3 main.py -m transformer -b 128 -d 1mb > slurm-tfLM-1mb-$SLURM_JOB_ID.out
+python3 main.py -m transformer -b 128 -d 1mb -g 0> slurm-tfLM-1mb-$SLURM_JOB_ID.out
